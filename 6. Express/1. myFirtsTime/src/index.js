e9f1cfd5ba6c4e119ab160c2744088f43a7288da
diego40g <diegopaznaula@hotmail.com>
2022-09-21 23:39:32 -0500

    validation of all petition of no has declaration

--- 6. Express/1. myFirtsTime/src/index.js
+++ 6. Express/1. myFirtsTime/src/index.js
@@ -1,13 +1,13 @@
-const express = require('express')
+const express = require('express');
 const {phone} = require('phone');
 
-const  PORT = 5000;
+const  PORT = 3000;
 
 const app = express();
 
 app.get("/",(req,res)=>{
     //with express
-    res.status(200).send("<html><head><meta charset=\"utf-8\"/></head><body>Servidor express !!!</body></html>")
+    res.status(200).send("<html><head><meta charset=\"utf-8\"/></head><body>Servidor express !!!</body></html>");
     /*with out express
     res.writeHead(200,{"Content-Type": "text/html"});
     res.write("<html><head><meta charset=\"utf-8\"/></head><body>Hola</body></html>");
@@ -15,26 +15,36 @@ app.get("/",(req,res)=>{
 });
 
 app.get("/info",(req,res) => {
-    res.setHeader("Content-type","application/json")
-    res.status(200).send(JSON.stringify({version:"0.0.1",appName:"Express server"}))
-})
+    res.setHeader("Content-type","application/json");
+    res.status(200).send(JSON.stringify({version:"0.0.1",appName:"Express server"}));
+});
 
 app.get("/detail",(req,res) => {
-    res.setHeader("Content-type","application/json")
-    res.status(200).send(JSON.stringify({version:"0.0.1",appName:"Express server"}))
-})
+    res.setHeader("Content-type","application/json");
+    res.status(200).send(JSON.stringify({version:"0.0.1",appName:"Express server"}));
+});
 
 app.get('/phone',(req,res)=>{
     const {value, country} = req.query;
     try{
         const countryPhone = phone(value, {country: country});
-        res.setHeader("Content-type","application/json")
+        res.setHeader("Content-type","application/json");
         res.status(200).send(JSON.stringify(countryPhone));
     }catch (e){
-        res.status(400).send("Peticion incorrecta: ",e.message)
+        res.status(400).send("Peticion incorrecta: ",e.message);
     }
-})
+});
+
+/*solo para peticiones get que no existen
+app.get("*",(req,res)=>{
+    res.status(404).send("Not Found");
+})*/
+
+//middleward
+app.use((req,res)=>{
+    res.status(404).send("Not Found");
+});
 
 app.listen(PORT, ()=>{
-    console.log("Ejecutandose en "+PORT);
+    console.log("Ejecutandose en " + PORT);
 })
