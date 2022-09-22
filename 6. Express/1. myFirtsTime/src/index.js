a4e2f98375a558ad982279e8f0c982383cf5609f
diego40g <diegopaznaula@hotmail.com>
2022-09-21 23:20:34 -0500

    first server with express app listen and petition get

--- 6. Express/1. myFirtsTime/src/index.js
+++ 6. Express/1. myFirtsTime/src/index.js
@@ -1,1 +1,18 @@
-cd 
+const express = require('express')
+
+const  PORT = 5000;
+
+const app = express();
+
+app.get("/",(req,res)=>{
+    //with express
+    res.status(200).send("<html><head><meta charset=\"utf-8\"/></head><body>Servidor express !!!</body></html>")
+    /*with out express
+    res.writeHead(200,{"Content-Type": "text/html"});
+    res.write("<html><head><meta charset=\"utf-8\"/></head><body>Hola</body></html>");
+    res.end();*/
+});
+
+app.listen(PORT, ()=>{
+    console.log("Ejecutandose en "+PORT);
+})
