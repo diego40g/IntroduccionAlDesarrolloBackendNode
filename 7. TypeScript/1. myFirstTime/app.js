5b3c644bf28c0d61c68e30041bb648e52d530a35
diego40g <diegopaznaula@hotmail.com>
2022-10-02 22:22:46 -0500

    first step with typescript and create js files

---
+++ 7. TypeScript/1. myFirstTime/app.js
@@ -0,0 +1,4 @@
+function addition(num1, num2) {
+    return num1 + num2;
+}
+console.log("La suma es: ", addition(3, 5));
---
+++ 7. TypeScript/1. myFirstTime/app.ts
@@ -0,0 +1,9 @@
+/* debe compilar para transaformar a js
+tsc app.ts*/
+
+function addition(num1:number, num2:number):number {
+    return num1+num2;
+}
+
+console.log("La suma es: ", addition(3,5));
+
