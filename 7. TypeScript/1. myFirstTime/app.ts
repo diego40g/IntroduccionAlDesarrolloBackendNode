4dcd36eb82d825b954d7c974d360a9f3a2ac4bfa
diego40g <diegopaznaula@hotmail.com>
2022-10-02 22:29:19 -0500

    compile automatic typescript to generate javascript

--- 7. TypeScript/1. myFirstTime/app.ts
+++ 7. TypeScript/1. myFirstTime/app.ts
@@ -1,6 +1,10 @@
 /* debe compilar para transaformar a js
 tsc app.ts*/
 
+/* Para evitar escribir siempre colocamos 
+tsc app.ts --watch
+tsc app.ts -w
+ */
 function addition(num1:number, num2:number):number {
     return num1+num2;
 }
