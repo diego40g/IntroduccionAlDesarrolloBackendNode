ba13fa0385216723fa1f184984c12ed44ca45d02
diego40g <diegopaznaula@hotmail.com>
2022-10-03 23:17:35 -0500

    inheritance for class functions, paramters  and constructors

---
+++ 7. TypeScript/7. inheritance/dist/app.js
@@ -0,0 +1,36 @@
+"use strict";
+class Animal {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    toString() {
+        return `name: ${this.name}, age: ${this.age}`;
+    }
+}
+class Mammal extends Animal {
+    //mas legible usar JSON
+    constructor(data) {
+        const { name, age, canSwim } = data;
+        super(name, age); //primero poner super porque debemos instanciar la clase padre
+        this.canSwim = canSwim;
+    }
+    swim() {
+        console.log(`${this.name} can swim ${this.canSwim}`);
+    }
+}
+class Bird extends Animal {
+    constructor(name, age, canFly) {
+        super(name, age); //constructor de la clase padre
+        this.canFly = canFly;
+    }
+    fly() {
+        console.log(`${this.name} can fly ${this.canFly}`);
+    }
+}
+const dog = new Mammal({ age: 3, canSwim: false, name: "Baylee" });
+const condor = new Bird("Loquillo", 10, true);
+console.log("mammal ", dog.toString());
+dog.swim();
+console.log("birth ", condor.toString());
+condor.fly();
