6f3ab2204933e7d341a4b7bb11882cf82034a97a
diego40g <diegopaznaula@hotmail.com>
2022-10-03 22:41:02 -0500

    generate typescript in specifict folders

---
+++ 7. TypeScript/4. InitTs/dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+//tsc --init
+//genera tsconfig.json
+let user = {
+    name: "Ignacio",
+    lastname: "Naula",
+    age: 22
+};
---
+++ 7. TypeScript/4. InitTs/src/app.ts
@@ -0,0 +1,14 @@
+//tsc --init
+//genera tsconfig.json
+
+type UserData = {
+    name: string;
+    lastname: string;
+    age: number;
+}
+
+let user: UserData = {
+    name: "Ignacio",
+    lastname: "Naula",
+    age: 22
+}
