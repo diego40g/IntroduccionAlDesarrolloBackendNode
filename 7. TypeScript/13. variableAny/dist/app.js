8bef8820f874fc4f776fb60004b7172d90c34456
diego40g <diegopaznaula@hotmail.com>
2022-10-09 23:28:39 -0500

    the diferents of use a data type ANY

---
+++ 7. TypeScript/13. variableAny/dist/app.js
@@ -0,0 +1,11 @@
+"use strict";
+/** USAR SOLO CUANDO NO SE CONOCE EL VALOR QUE DEVUELVE UN SDK  */
+/* SIEMPRE USAR LA DECLARACION DEL DATO */
+let user; //es tipo any (generico) que puede cambiar en tiempo de ejecucion
+user = "Diego";
+user = 12;
+user = false;
+user = null;
+let variable = "D"; //no puede cambiar en tiempo de ejecucion
+//varaible=2;
+//variable=null; no admite datos null
---
+++ 7. TypeScript/13. variableAny/src/app.ts
@@ -0,0 +1,11 @@
+/** USAR SOLO CUANDO NO SE CONOCE EL VALOR QUE DEVUELVE UN SDK  */
+/* SIEMPRE USAR LA DECLARACION DEL DATO */
+let user:any; //es tipo any (generico) que puede cambiar en tiempo de ejecucion
+user="Diego";
+user=12;
+user=false;
+user=null;
+
+let variable="D";//no puede cambiar en tiempo de ejecucion
+//varaible=2;
+//variable=null; no admite datos null
