ee7892b0ddd6c43bd41e5d5666a76cc3f248b24f
diego40g <diegopaznaula@hotmail.com>
2022-10-28 19:48:19 -0500

    method or

--- 9. Express-MongoDB/1. Mongo/script.js
+++ 9. Express-MongoDB/1. Mongo/script.js
@@ -33,3 +33,31 @@ db.nombre_collection.find({code:{$ne:"EC"}})
 db.nombre_collection.find({code:"CO", name:"Colombia"})
 db.nombre_collection.find({$and:[{code:"EC", name:{$ne:"Ecuador"}}]})
 db.nombre_collection.find({$and:[{code:"CO"}, {name:{$ne:"Ecuador"}}]})
+
+
+db.products.insert([
+    {
+        name:'Flutter',
+        price: 19.99
+    },{
+        name:'React',
+        price: 29.99,
+    },{
+        name:'Android',
+        price: 99.99,
+        note: 'Great'
+    },{
+        name:'NodeJS',
+        price: 9.99,
+        note: 'Great'
+    }
+]);
+//mayor que ($gt)
+db.products.find({price:{$gt:9.99}})
+//mayor o igual ($gte)
+db.products.find({price:{$gte:9.99}})
+db.products.find({$or:[{name:'Flutter'}, {price:{$gt:9.99}}]})
+
+/*url documentation
+https://www.mongodb.com/docs/manual/reference/operator/query/
+*/
